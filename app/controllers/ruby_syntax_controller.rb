19beac732dda13446b6be9e2945acccb71c35bc5
Todor Chakarov <tosheee@abv.bg>
2016-03-24 00:10:16 +0200

    Fix index admin panel

--- app/controllers/ruby_syntax_controller.rb
+++ app/controllers/ruby_syntax_controller.rb
@@ -5,16 +5,10 @@ class RubySyntaxController < ApplicationController
   end
 
   def show
-    @syntax_elements_rubies = SyntaxElementsRuby.all#vertical nav
-
-    @syntax_elements_ruby = SyntaxElementsRuby.find(params[:id])#dumata gore
-
+    @syntax_elements_rubies = SyntaxElementsRuby.all
+    @syntax_elements_ruby = SyntaxElementsRuby.find(params[:id])
     @syntax_keyword_rubies = SyntaxKeywordRuby.where(syntax_elements_rubies_id: @syntax_elements_ruby).pluck(:id, :syntax_keyword)
-
-    #@description_keyword = DescriptionKeyword.all
-
-
-    #@syntax_keyword_rubies_id = SyntaxKeywordRuby.where(syntax_elements_rubies_id: @syntax_elements_ruby).pluck(:syntax_elements_rubies_id)
+    @description_keyword = DescriptionKeyword.all
   end
 
 end
